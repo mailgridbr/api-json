8a3fccaed56ed8d6de485565ffe11b0a2577bcb3
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2022-09-27 16:29:20 -0300

    Create relatorio.py

---
+++ python/relatorio.py
@@ -0,0 +1,15 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+
+url = "https://painel.mailgrid.com.br/api/report.php"
+
+headers = CaseInsensitiveDict()
+headers["Authorization"] = "Content-Type: application/json"
+headers["Content-Type"] = "application/json"
+
+data = '{"usuario_smtp": "USUARIO-SMTP", "senha_smtp": "SENHA-SMTP", "dataini": "2022-09-27", "horaini": "00:01", "datafim": "2022-09-27", "horafim": "23:59"}'
+
+
+resp = requests.post(url, headers=headers, data=data)
+
+print(resp.status_code)
