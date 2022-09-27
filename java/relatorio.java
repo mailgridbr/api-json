4941e52009ed6cc94ecfd712e5232426d2bef62e
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2022-09-27 16:30:39 -0300

    Create relatorio.java

---
+++ java/relatorio.java
@@ -0,0 +1,16 @@
+URL url = new URL("https://painel.mailgrid.com.br/api/report.php");
+HttpURLConnection http = (HttpURLConnection)url.openConnection();
+http.setRequestMethod("POST");
+http.setDoOutput(true);
+http.setRequestProperty("Authorization", "Content-Type: application/json");
+http.setRequestProperty("Content-Type", "application/json");
+
+String data = "{\"usuario_smtp\": \"USUARIO-SMTP\", \"senha_smtp\": \"SENHA-SMTP\", \"dataini\": \"2022-09-27\", \"horaini\": \"00:01\", \"datafim\": \"2022-09-27\", \"horafim\": \"23:59\"}";
+
+byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+OutputStream stream = http.getOutputStream();
+stream.write(out);
+
+System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+http.disconnect();
