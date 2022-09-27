bf59db9993dfd77fac578208e698158f028442a0
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2022-09-27 15:46:43 -0300

    Create envio.js

---
+++ javascript/envio.js
@@ -0,0 +1,18 @@
+var url = "https://painel.mailgrid.com.br/api/";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader("Authorization", "Content-Type: application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+var data = `{"host_smtp": "HOST-SMTP","usuario_smtp": "USUARIO-SMTP", "senha_smtp": "SENHA-SMTP", "emailRemetente": "EMAIL-REMETENTE", "nomeRemetente": "NOME-REMETENTE","emailDestino": ["postmaster@mailgrid.com.br","dev@mailgrid.com.br"], "assunto": "Teste de envio via API JSON",
+"mensagem": "mensagem de teste da API JSON"}`;
+
+xhr.send(data);
