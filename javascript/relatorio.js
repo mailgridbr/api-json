1b9e84a18f3a1018b11383575d25a808d55dc033
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2022-09-27 16:30:01 -0300

    Create relatorio.js

---
+++ javascript/relatorio.js
@@ -0,0 +1,17 @@
+var url = "https://painel.mailgrid.com.br/api/report.php";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader("Authorization", "Content-Type: application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+var data = '{"usuario_smtp": "USUARIO-SMTP", "senha_smtp": "SENHA-SMTP", "dataini": "2022-09-27", "horaini": "00:01", "datafim": "2022-09-27", "horafim": "23:59"}';
+
+xhr.send(data);
