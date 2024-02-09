8b9bc380abe21ca57f9969b93482b27c6bbee57b
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2024-02-08 21:51:49 -0300

    Update envio.sh

--- curl/envio.sh
+++ curl/envio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -X POST https://painel.mailgrid.com.br/api/ -H "Authorization: Content-Type: application/json" -H "Content-Type: application/json" --data-binary @- <<DATA
+curl -X POST https://painel.mailgrid.com.br/api/send/ -H "Authorization: Content-Type: application/json" -H "Content-Type: application/json" --data-binary @- <<DATA
 {"host_smtp": "HOST-SMTP","usuario_smtp": "USUARIO-SMTP", "senha_smtp": "SENHA-SMTP", "emailRemetente": "EMAIL-REMETENTE", "nomeRemetente": "NOME-REMETENTE","emailDestino": ["postmaster@mailgrid.com.br","dev@mailgrid.com.br"], "assunto": "Teste de envio via API JSON",
 "mensagem": "mensagem de teste da API JSON"}
 DATA
