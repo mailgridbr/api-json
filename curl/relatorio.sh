6ae01bc98bf64900ea9c17713412e50efa1440f7
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2024-07-04 16:54:27 -0300

    Update relatorio.sh

--- curl/relatorio.sh
+++ curl/relatorio.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST https://painel.mailgrid.com.br/api/report/ -H "Authorization: Content-Type: application/json" -H "Content-Type: application/json" -d "{\"usuario_smtp\": \"USUARIO-SMTP\", \"senha_smtp\": \"SENHA-SMTP\", \"dataini\": \"2022-09-27\", \"horaini\": \"00:01\", \"datafim\": \"2022-09-27\", \"horafim\": \"23:59\"}"
+curl -X POST https://api.mailgrid.net.br/report/ -H "Authorization: Content-Type: application/json" -H "Content-Type: application/json" -d "{\"usuario_smtp\": \"USUARIO-SMTP\", \"senha_smtp\": \"SENHA-SMTP\", \"dataini\": \"2022-09-27\", \"horaini\": \"00:01\", \"datafim\": \"2022-09-27\", \"horafim\": \"23:59\"}"
