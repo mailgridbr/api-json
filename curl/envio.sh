08fb062f626f54dacf8e04b27c4e421ee2954980
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2025-09-18 08:32:11 -0300

    Update envio.sh

--- curl/envio.sh
+++ curl/envio.sh
@@ -1,36 +1,32 @@
-#!/bin/bash
+# Enviar um e-mail com anexos usando a API mailgrid com cURL
 
-# Defina a URL da API de envio de e-mail
-API_URL="https://api.mailgrid.net.br/send/"
-
-# Defina os parâmetros de autenticação e envio do e-mail
-HOST_SMTP="HOST-SMTP"  # Substitua pelo seu host SMTP
-USUARIO_SMTP="USUARIO-SMTP"  # Substitua pelo seu usuário SMTP
-SENHA_SMTP="SENHA-SMTP"  # Substitua pela sua senha SMTP
-EMAIL_REMETENTE="EMAIL-REMETENTE"  # Substitua pelo e-mail do remetente
-NOME_REMETENTE="NOME-REMETENTE"  # Substitua pelo nome do remetente
-EMAIL_DESTINOS=("postmaster@mailgrid.com.br" "dev@mailgrid.com.br")  # Lista de destinatários do e-mail
-ASSUNTO="Teste de envio via API JSON"  # Assunto do e-mail
-MENSAGEM="mensagem de teste da API JSON"  # Corpo da mensagem do e-mail
-
-# Crie o corpo do JSON para a requisição
-JSON_DATA=$(cat <<EOF
-{
-  "host_smtp": "$HOST_SMTP",
-  "usuario_smtp": "$USUARIO_SMTP",
-  "senha_smtp": "$SENHA_SMTP",
-  "emailRemetente": "$EMAIL_REMETENTE",
-  "nomeRemetente": "$NOME_REMETENTE",
-  "emailDestino": $(echo ${EMAIL_DESTINOS[@]} | sed 's/ /","/g'),
-  "assunto": "$ASSUNTO",
-  "mensagem": "$MENSAGEM"
-}
-EOF
-)
-
-# Envie a requisição POST para a API utilizando o curl
-curl -X POST "$API_URL" \
-  -H "Authorization: Content-Type: application/json" \  # Cabeçalho de autorização e tipo de conteúdo
-  -H "Content-Type: application/json" \  # Define o tipo de conteúdo como JSON
-  --data-binary "$JSON_DATA"  # Envia os dados JSON como corpo da requisição
-
+curl --location 'https://api.mailgrid.net.br/sendmail/' \  # Endpoint da API para envio de e-mails
+	--header 'Content-Type: application/json' \ # Define o cabeçalho para indicar que o corpo da requisição é em JSON
+	--data-raw '{
+	"host_smtp": "HOST-SMTP",  # Substitua pelo host SMTP do serviço de e-mail
+	"usuario_smtp": "USUARIO-SMTP",  # Substitua pelo seu usuário SMTP para autenticação
+	"senha_smtp": "SENHA-SMTP",  # Substitua pela senha SMTP para autenticação
+	"emailRemetente": "EMAIL-REMETENTE",  # Substitua pelo e-mail do remetente
+	"nomeRemetente": "NOME-REMETENTE",  # Substitua pelo nome do remetente
+	"emailDestino": ["dev@mailgrid.net.br", "postmaster@mailgrid.net.br"],  # Lista de destinatários
+	"assunto": "Teste da API JSON com anexo",  # Assunto do e-mail
+	"mensagemAnexos": [
+		{
+			"name": "pixel.jpg",  # Nome do arquivo no e-mail
+			"type": "image/jpeg",  # Tipo MIME do arquivo (neste caso, imagem JPEG)
+			"content": "/9j/4AAQSkZJRgABAQEBLAEsAAD/2wBDAAYEBAQFBAYFBQYJBgUGCQsIBgYICwwKCgsKCgwQDA...."
+		}, 
+		{
+			"name": "pixel2.jpg",  # Nome do segundo arquivo no e-mail
+			"type": "image/jpeg",  # Tipo MIME do segundo arquivo (imagem JPEG)
+			"content": "/9j/4AAQSkZJRgABAQEBLAEsAAD/2wBDAAYEBAQFBAYFBQYJBgUGCQsIBgYICwwKCgsKCgwQDA...."
+		}
+	],
+	"mensagem": "Mensagem de teste da API com anexos. Testando anexos de html no envio da api", 
+	# Corpo da mensagem em HTML
+	"mensagemTipo": "html",  # Define que o tipo da mensagem é HTML
+	# Codificação da mensagem para garantir a integridade dos caracteres especiais
+	"mensagemEncoding": "quoted-printable",
+	# Texto alternativo caso o HTML não seja renderizado corretamente
+	"mensagemAlt": "mensagem de teste da API JSON com anexos"
+}'
