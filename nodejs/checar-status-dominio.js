48d12fa21eda7622b0e1340d9f249f636d87d8a1
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2025-09-18 08:40:43 -0300

    Update checar-status-dominio.js

--- nodejs/checar-status-dominio.js
+++ nodejs/checar-status-dominio.js
@@ -5,35 +5,35 @@ const url = "https://api.mailgrid.net.br/domain/check/";
 
 // Dados de autenticação e domínio a ser checado
 const data = {
-    "token_auth": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // Token de autenticação obtido no painel do cliente
-    "dominio": "dominioaserchecado.net.br" // Domínio a ser verificado
+	"token_auth": "SEU_TOKEN_AQUI", // Token de autenticação obtido no painel do cliente
+	"dominio": "dominioaserchecado.net.br" // Domínio a ser verificado
 };
 
 // Configuração da requisição
 const config = {
-    headers: {
-        'Content-Type': 'application/json' // Define o cabeçalho da requisição como JSON
-    }
+	headers: {
+		'Content-Type': 'application/json' // Define o cabeçalho da requisição como JSON
+	}
 };
 
 // Envia a requisição POST
 axios.post(url, data, config)
-    .then((response) => {
-        // Se a resposta for bem-sucedida, exibe a resposta formatada
-        console.log("Resposta da API:");
-        console.log(JSON.stringify(response.data, null, 2)); // Exibe a resposta formatada
-    })
-    .catch((error) => {
-        // Caso ocorra erro na requisição
-        if (error.response) {
-            // A requisição foi feita e o servidor respondeu com um status de erro
-            console.log("Erro na requisição. Código de status HTTP: " + error.response.status);
-            console.log("Resposta do servidor:", error.response.data);
-        } else if (error.request) {
-            // A requisição foi feita, mas não houve resposta
-            console.log("Erro na requisição. Sem resposta do servidor:", error.request);
-        } else {
-            // Erro ao configurar a requisição
-            console.log("Erro ao configurar a requisição:", error.message);
-        }
-    });
+ .then((response) => {
+	// Se a resposta for bem-sucedida, exibe a resposta formatada
+	console.log("Resposta da API:");
+	console.log(JSON.stringify(response.data, null, 2)); // Exibe a resposta formatada
+ })
+ .catch((error) => {
+	// Caso ocorra erro na requisição
+	if (error.response) {
+		// A requisição foi feita e o servidor respondeu com um status de erro
+		console.log("Erro na requisição. Código de status HTTP: " + error.response.status);
+		console.log("Resposta do servidor:", error.response.data);
+	} else if (error.request) {
+		// A requisição foi feita, mas não houve resposta
+		console.log("Erro na requisição. Sem resposta do servidor:", error.request);
+	} else {
+		// Erro ao configurar a requisição
+		console.log("Erro ao configurar a requisição:", error.message);
+	}
+});
