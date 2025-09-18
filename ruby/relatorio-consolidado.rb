cfd2d3c59855aea3ecf51c2c4f0104ab481a8241
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2025-09-18 08:51:59 -0300

    Update relatorio-consolidado.rb

--- ruby/relatorio-consolidado.rb
+++ ruby/relatorio-consolidado.rb
@@ -7,12 +7,11 @@ url = URI("https://api.mailgrid.net.br/consolidate/")
 
 # Dados de autenticação e parâmetros de consulta
 data = {
-  "usuario_smtp" => "smtp1@seudominio.com.br",  # Usuário SMTP para autenticação
-  "senha_smtp" => "xxxxxxxx",  # Senha SMTP para autenticação
-  "dataini" => "2023-02-01",  # Data de início da consulta (AAAA-MM-DD)
-  "horaini" => "00:01",  # Hora de início da consulta (HH:MM)
-  "datafim" => "2023-08-30",  # Data de fim da consulta (AAAA-MM-DD)
-  "horafim" => "23:59"  # Hora de fim da consulta (HH:MM)
+	"token_auth" => "INFORME-O-TOKEN-AQUI",  # Token de autenticação
+	"dataini" => "2023-02-01",  # Data de início da consulta (AAAA-MM-DD)
+	"horaini" => "00:01",  # Hora de início da consulta (HH:MM)
+	"datafim" => "2023-08-30",  # Data de fim da consulta (AAAA-MM-DD)
+	"horafim" => "23:59"  # Hora de fim da consulta (HH:MM)
 }
 
 # Configuração da requisição HTTP
@@ -25,16 +24,16 @@ response = http.request(request)
 
 # Verifica o status da resposta
 if response.code.to_i != 200
-  # Se o código de status não for 200, houve um erro
-  puts "Erro na requisição. Código de status HTTP: #{response.code}"
+	# Se o código de status não for 200, houve um erro
+	puts "Erro na requisição. Código de status HTTP: #{response.code}"
 else
-  # Caso a resposta seja bem-sucedida, decodifica e exibe o JSON
-  begin
-    decoded_response = JSON.parse(response.body)
-    puts "Resposta da API:"
-    puts JSON.pretty_generate(decoded_response)  # Exibe a resposta formatada
-  rescue JSON::ParserError
-    # Caso não consiga processar o JSON, exibe um erro
-    puts "Erro ao processar a resposta JSON."
-  end
+	# Caso a resposta seja bem-sucedida, decodifica e exibe o JSON
+	begin
+	decoded_response = JSON.parse(response.body)
+	puts "Resposta da API:"
+	puts JSON.pretty_generate(decoded_response)  # Exibe a resposta formatada
+	rescue JSON::ParserError
+	# Caso não consiga processar o JSON, exibe um erro
+	puts "Erro ao processar a resposta JSON."
+	end
 end
