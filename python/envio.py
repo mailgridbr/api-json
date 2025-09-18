21237e88f782962e7909c65986fc7663456c8597
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2025-09-18 08:47:55 -0300

    Update envio.py

--- python/envio.py
+++ python/envio.py
@@ -1,38 +1,52 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-# Definindo a URL do endpoint da API MailGrid para envio de emails
-url = "https://api.mailgrid.net.br/send/"
+# URL do endpoint da API para envio de emails
+url = "https://api.mailgrid.net.br/sendmail/"
 
-# Criando um dicionário para os cabeçalhos HTTP da requisição
+# Criando um dicionário de cabeçalhos para a requisição HTTP
 headers = CaseInsensitiveDict()
-headers["Authorization"] = "Content-Type: application/json"  # Cabeçalho de autorização, que deve ser ajustado conforme necessário
-headers["Content-Type"] = "application/json"  # Cabeçalho que define o tipo de conteúdo como JSON
+headers["Content-Type"] = "application/json"  # Definindo o tipo de conteúdo como JSON
 
-# Dados para a requisição POST - Incluindo detalhes do SMTP, remetente, destinatários, assunto e corpo da mensagem
+# Dados para a requisição POST - Incluindo detalhes do SMTP, remetente, destinatário, anexos e mensagem
 data = """
 {
-    "host_smtp": "HOST-SMTP",  # Endereço do servidor SMTP
-    "usuario_smtp": "USUARIO-SMTP",  # Usuário de autenticação SMTP
-    "senha_smtp": "SENHA-SMTP",  # Senha de autenticação SMTP
-    "emailRemetente": "EMAIL-REMETENTE",  # Email do remetente
-    "nomeRemetente": "NOME-REMETENTE",  # Nome do remetente
-    "emailDestino": ["postmaster@mailgrid.com.br", "dev@mailgrid.com.br"],  # Lista de emails destinatários
-    "assunto": "Teste de envio via API JSON",  # Assunto do email
-    "mensagem": "mensagem de teste da API JSON"  # Corpo do email
+	"host_smtp": "HOST-SMTP",  # Endereço do servidor SMTP
+	"usuario_smtp": "USUARIO-SMTP",  # Usuário de autenticação SMTP
+	"senha_smtp": "SENHA-SMTP",  # Senha de autenticação SMTP
+	"emailRemetente": "EMAIL-REMETENTE",  # Email do remetente
+	"nomeRemetente": "NOME-REMETENTE",  # Nome do remetente
+	"emailDestino": ["dev@mailgrid.net.br", "postmaster@mailgrid.net.br"],  # Lista de emails destinatários
+	"assunto": "Teste da API JSON com anexo",  # Assunto do email
+	"mensagemAnexos": [
+	{
+		"name": "pixel.jpg",  # Nome do primeiro anexo
+		"type": "image/jpeg",  # Tipo do arquivo (MIME type)
+		"content": "/9j/4AAQSkZJRgABAQEBLAEsAAD/2wBDAAYEBAQFBAYFBQY..."  # Conteúdo do arquivo codificado em base64
+	},
+	{
+		"name": "pixel2.jpg",  # Nome do segundo anexo
+		"type": "image/jpeg",  # Tipo do arquivo (MIME type)
+		"content": "/9j/4AAQSkZJRgABAQEBLAEsAAD/2wBDAAYEBAQFBAYFBQY..."  # Conteúdo do arquivo codificado em base64
+		}
+	],
+	"mensagem": "Mensagem de teste da API com anexos. Testando anexos de html no envio da api",  # Corpo da mensagem em HTML
+	"mensagemTipo": "html",  # Tipo da mensagem (HTML)
+	"mensagemEncoding": "quoted-printable",  # Encoding da mensagem
+	"mensagemAlt": "mensagem de teste da API JSON com anexos"  # Versão alternativa da mensagem
 }
 """
 
 # Fazendo a requisição POST para o endpoint com os cabeçalhos e os dados JSON
 resp = requests.post(url, headers=headers, data=data)
 
-# Verificando o código de status da resposta e exibindo o código de status HTTP
+# Verificando o status da resposta e exibindo o código de status HTTP
 print(resp.status_code)
 
-# Se a requisição for bem-sucedida (código 200), pode-se processar a resposta
+# Se a resposta for bem-sucedida (status code 200), pode-se processar o conteúdo da resposta
 if resp.status_code == 200:
-    print("Requisição bem-sucedida!")  # Se a resposta for bem-sucedida
-    print(resp.json())  # Exibe a resposta JSON da API
+	print("Requisição bem-sucedida!")
+	print(resp.json())  # Exibe a resposta JSON da API
 else:
-    # Caso ocorra um erro, exibe o erro com base no código de status
-    print(f"Erro na requisição: {resp.text}")
+	print(f"Erro na requisição: {resp.text}")  # Exibe o erro caso a requisição falhe
+								
