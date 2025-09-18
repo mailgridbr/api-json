ef720b7b19f08379e3aa4eda19afe9d03dc22fa9
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2025-09-18 08:29:23 -0300

    Update adicionar-dominio-remetente.sh

--- curl/adicionar-dominio-remetente.sh
+++ curl/adicionar-dominio-remetente.sh
@@ -2,7 +2,7 @@
 URL="https://api.mailgrid.net.br/domain/add/"
 
 # Token de autenticação e domínio a ser adicionado
-TOKEN_AUTH="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  # Substitua com seu token de autenticação
+TOKEN_AUTH="SEU_TOKEN_AQUI"  # Substitua com seu token de autenticação
 DOMINIO="dominioaseradicionado.net.br"  # Substitua com o domínio a ser adicionado
 
 # Dados a serem enviados no corpo da requisição em formato JSON
@@ -10,9 +10,9 @@ DATA="{\"token_auth\":\"$TOKEN_AUTH\", \"dominio\":\"$DOMINIO\"}"
 
 # Requisição cURL
 curl -X POST "$URL" \  # Método POST
-    -H "Content-Type: application/json" \  # Define o cabeçalho Content-Type
-    -d "$DATA" \  # Dados JSON enviados no corpo da requisição
-    --timeout 30 \  # Define o tempo limite da requisição (30 segundos)
-    --ssl-verify  # Verifica o SSL da conexão (padrão de segurança)
+	-H "Content-Type: application/json" \  # Define o cabeçalho Content-Type
+	-d "$DATA" \  # Dados JSON enviados no corpo da requisição
+	--timeout 30 \  # Define o tempo limite da requisição (30 segundos)
+	--ssl-verify  # Verifica o SSL da conexão (padrão de segurança)
 
 # A resposta será exibida diretamente no terminal, incluindo erros se houver
