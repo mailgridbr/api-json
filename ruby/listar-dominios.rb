c1996a58b6dcca46b8297ef3c792d5b789cca0f3
MailGrid Dev <114513545+mailgridbr@users.noreply.github.com>
2025-09-18 08:51:37 -0300

    Update listar-dominios.rb

--- ruby/listar-dominios.rb
+++ ruby/listar-dominios.rb
@@ -7,7 +7,7 @@ url = URI("https://api.mailgrid.net.br/domain/list/")
 
 # Dados de autenticação (substitua pelo seu token real)
 data = {
-  "token_auth" => "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # Token obtido no painel do cliente
+	"token_auth" => "SEU_TOKEN_AQUI" # Token obtido no painel do cliente
 }
 
 # Configuração da requisição HTTP
@@ -20,16 +20,16 @@ response = http.request(request)
 
 # Verifica o status da resposta
 if response.code.to_i >= 400
-  # Se o status for maior ou igual a 400, houve erro na requisição
-  puts "Erro na requisição. Código de status HTTP: #{response.code}"
+	# Se o status for maior ou igual a 400, houve erro na requisição
+	puts "Erro na requisição. Código de status HTTP: #{response.code}"
 else
-  # Caso a resposta seja bem-sucedida, decodifica e exibe o JSON
-  begin
-    decoded_response = JSON.parse(response.body)
-    puts "Resposta da API:"
-    puts JSON.pretty_generate(decoded_response) # Exibe a resposta formatada
-  rescue JSON::ParserError
-    # Caso não consiga processar o JSON, exibe um erro
-    puts "Erro ao processar a resposta JSON."
-  end
+	# Caso a resposta seja bem-sucedida, decodifica e exibe o JSON
+	begin
+	decoded_response = JSON.parse(response.body)
+	puts "Resposta da API:"
+	puts JSON.pretty_generate(decoded_response) # Exibe a resposta formatada
+	rescue JSON::ParserError
+	# Caso não consiga processar o JSON, exibe um erro
+	puts "Erro ao processar a resposta JSON."
+	end
 end
